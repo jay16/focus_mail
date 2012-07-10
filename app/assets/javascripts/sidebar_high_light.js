eb955fe55a88df274fe2fd4e1638784c7519d55d
jay_li <jay_li@intfocus.com>
2012-07-10 21:02:41 +0800

    set li more flexibility

--- app/assets/javascripts/sidebar_high_light.js
+++ app/assets/javascripts/sidebar_high_light.js
@@ -1,32 +1,29 @@
- function setStyle(id){
-   $(id).css({"font-size":"140%","color":"blue"});
-  }     
- var arr=location.pathname.split("/");
-  //alert(arr[1]);
-	switch(arr[1])
-  {
-   case "":
-			  $(".nav-home").css({"font-size":"140%","color":"white"});
-				 break;
-			case "generate_email":
-					setStyle("#nav-generate-email");
-			  break;
-			case "lists":
-					setStyle("#nav-lists");
-			  break;
-			case "templates":
-					setStyle("#nav-templates");
-			  break;
-			case "campaigns":
-					setStyle("#nav-campaigns");
-			  break;
-			case "clicks":
-					setStyle("#nav-clicks");
-			  break;
-			case "tracks":
-					setStyle("#nav-tracks");
-			  break;
-   default:
-   alert("where do you click?!") 
-  }
+function setStyle(id) { $(id).css({"font-size":"140%","color":"blue"}); }     
+var arr=location.pathname.split("/");
+switch(arr[1])
+ {
+  case "":
+		  $(".nav-home").css({"font-size":"140%","color":"white"});
+			 break;
+		case "generate_email":
+				setStyle("#nav-generate-email");
+		  break;
+		case "lists":
+				setStyle("#nav-lists");
+		  break;
+		case "templates":
+				setStyle("#nav-templates");
+		  break;
+		case "campaigns":
+				setStyle("#nav-campaigns");
+		  break;
+		case "clicks":
+				setStyle("#nav-clicks");
+		  break;
+		case "tracks":
+				setStyle("#nav-tracks");
+		  break;
+  default:
+  alert("where do you click?!") 
+ }
 
